7f1588e769bfca6007c8c17b12c0d886f05cc3f4
miskaso <qwertyclop@gmail.com>
2024-07-26 18:55:33 +0600

    new

--- main/views.py
+++ main/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Product
+from django.views.generic import ListView
 
 # Create your views here.
 
@@ -6,3 +8,12 @@ from django.shortcuts import render
 def index(req):
     return render(req, "index.html")
 
+
+# class ProductView(ListView):
+#     template_name = 'index.html'
+#     model = Product
+#     context_object_name = 'products'
+#     paginate_by = 10
+#
+#     def get_context_data(self, *, object_list=None, **kwargs):
+#         context = super().get
