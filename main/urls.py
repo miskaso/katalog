508c99cbae869091e68a92642136cda97ddd5c85
deonira <mada_050397@mail.ru>
2024-07-26 19:53:45 +0600

    123

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path, include
 from views import product_list
+from . import views
 urlpatterns = [
-    path('', product_list, name='product_list')
+    path('', product_list, name='product_list'),
+    path('register/', views.register_view, name='register'),
+    path('login/', views.login_view, name='login'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from .models import Product
-
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
+from .forms import CustomUserCreationForm, CustomAuthenticationForm
+@login_required
 def product_list(request):
     category = request.GET.get('category')
     products = Product.object.all()
@@ -9,7 +12,27 @@ def product_list(request):
     return render(request, 'product_list.html', {'products': products})
 
 
+def register_view(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('guestbook')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'register.html', {'form': form})
 
+def login_view(request):
+    if request.method == 'POST':
+        form = CustomAuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = authenticate(username=form.cleaned_data['username'], password=form.cleaned_data['password'])
+            if user is not None:
+                login(request, user)
+                return redirect('guestbook')
+    else:
+        form = CustomAuthenticationForm()
+    return render(request, 'login.html', {'form': form})
 
 
 # class ProductView(ListView):
