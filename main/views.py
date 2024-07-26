584d10d6512b72a47a9ee4c6cac49d0e25e1a0f2
miskaso <qwertyclop@gmail.com>
2024-07-26 20:10:15 +0600

    new

--- main/views.py
+++ main/views.py
@@ -7,6 +7,7 @@ from .forms import CustomUserCreationForm, CustomAuthenticationForm
 
 from django.views.generic import DetailView
 
+
 @login_required
 def product_list(request):
     category = request.GET.get('category')
@@ -22,10 +23,11 @@ def register_view(request):
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('guestbook')
+            return redirect('home')
     else:
         form = CustomUserCreationForm()
     return render(request, 'register.html', {'form': form})
+
 
 def login_view(request):
     if request.method == 'POST':
@@ -34,7 +36,7 @@ def login_view(request):
             user = authenticate(username=form.cleaned_data['username'], password=form.cleaned_data['password'])
             if user is not None:
                 login(request, user)
-                return redirect('guestbook')
+                return redirect('home')
     else:
         form = CustomAuthenticationForm()
     return render(request, 'login.html', {'form': form})
