9e1cae4965aa33626f7841274470a93b1c68f0df
deonira <mada_050397@mail.ru>
2024-07-28 01:14:06 +0600

    123

--- main/models.py
+++ main/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.utils.text import slugify
 # Create your models here.
 
 
@@ -8,7 +8,18 @@ class Product(models.Model):
     description = models.TextField()
     category = models.CharField(max_length=55)
     cena = models.IntegerField()
-    slug = models.SlugField(null=False, unique='detail')
+    slug = models.SlugField(unique=True, blank=True)
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            base_slug = slugify(self.name)
+            slug = base_slug
+            num = 1
+            while Product.objects.filter(slug=slug).exists():
+                slug = f"{base_slug}-{num}"
+                num += 1
+            self.slug = slug
+        super().save(*args, **kwargs)
